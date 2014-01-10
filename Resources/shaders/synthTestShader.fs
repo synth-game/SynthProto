72f13a12293f942b7d3f8e3fec2b9406ab4d8719
Jijidici <jdefaye.jijidici@gmail.com>
2014-01-10 20:42:30 +0100

    lighting shader: fix bug on raycast step

--- Resources/shaders/synthTestShader.fs
+++ Resources/shaders/synthTestShader.fs
@@ -10,7 +10,9 @@ uniform vec2 uTexSize;
 void main()
 {
 	vec2 pixelStep = vec2(1./uTexSize.x, 1./uTexSize.y);
-
+	float meanPixelStep = (pixelStep.x + pixelStep.y) /2.;
+	
+	
 	//light setup	
 	vec2 lightPos1 = vec2(490.*pixelStep.x, 260.*pixelStep.y);
 	vec2 lightPos2 = vec2(540.*pixelStep.x, 260.*pixelStep.y);
@@ -34,7 +36,7 @@ void main()
 			float fDotTest = 1.;
 			
 			while(fDotTest > 0.) {
-				samplePos = samplePos + vec2(-LF.x*pixelStep.x, -LF.y*pixelStep.y);
+				samplePos = samplePos + vec2(-LF.x*meanPixelStep, -LF.y*meanPixelStep);
 				fDotTest = dot(lightPos1-samplePos, lightPos1-v_texCoord);
 				
 				vec4 texSample = texture2D(CC_Texture0, samplePos);
@@ -60,7 +62,7 @@ void main()
 			float fDotTest = 1.;
 			
 			while(fDotTest > 0.) {
-				samplePos = samplePos + vec2(-LF.x*pixelStep.x, -LF.y*pixelStep.y);
+				samplePos = samplePos + vec2(-LF.x*meanPixelStep, -LF.y*meanPixelStep);
 				fDotTest = dot(lightPos2-samplePos, lightPos2-v_texCoord);
 				
 				vec4 texSample = texture2D(CC_Texture0, samplePos);
